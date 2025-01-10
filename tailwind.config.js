1e4c4e7a73bc76ab23e06f29a6f91a43177816e2
De1oNn <nasanjargal20040524@gmail.com>
2025-01-10 13:01:16 +0800

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark': '#0F141E',
+        'custom-white': '#F3F4F6'
+      }
+    },
+  },
+  plugins: [],
+}
+
